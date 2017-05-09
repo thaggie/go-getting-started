328d03e993f3971bb07bbbbbddd7f4e08de39501
Tom Haggie <thaggie@gmail.com>
2017-05-08 23:19:45 -0700

    Added repeat

--- main.go
+++ main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/russross/blackfriday"
 )
 
+var (
+    repeat int
+)
+
+func repeatHandler(c *gin.Context) {
+    var buffer bytes.Buffer
+    for i := 0; i < repeat; i++ {
+        buffer.WriteString("Hello from Go!!!\n")
+    }
+    c.String(http.StatusOK, buffer.String())
+}
+
 func main() {
+	var err error
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
+	}
+
+	tStr := os.Getenv("REPEAT")
+  repeat, err = strconv.Atoi(tStr)
+	if err != nil {
+  	log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
+  	repeat = 5
 	}
 
 	router := gin.New()
@@ -24,7 +46,9 @@ func main() {
 	router.GET("/mark", func(c *gin.Context) {
 	  c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
 	})
-	
+
+	router.GET("/repeat", repeatHandler)
+
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
